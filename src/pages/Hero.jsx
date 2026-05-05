304e56aa41774577cac41acd8076ad403167258a
Sunil Web-Work <suniltariq08@gmail.com>
2026-05-05 22:46:16 +0500

    fixed

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { motion } from "framer-motion";
 import { FiGithub, FiLinkedin, FiMail, FiDownload, FiArrowRight } from 'react-icons/fi';
 import { FaClock, FaRocket, FaSmile, FaLaptopCode } from "react-icons/fa";
